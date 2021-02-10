4d140dbbeee00e7d730460eaf78a41e0a344b609
Martin Stoyanov <martinestoyanov@gmail.com>
2021-02-10 17:50:49 -0500

    working start/stop button

--- script.js
+++ script.js
@@ -1,16 +1,65 @@
+// mole.src = "https://assets.pokemon.com/assets/cms2/img/pokedex/full/050.png";
+// startGame();
+
+///vars///
 let mole = document.createElement("img");
 mole.classList.add("mole");
-let score = 0;
-// mole.src = "https://assets.pokemon.com/assets/cms2/img/pokedex/full/050.png";
+// let score = 0;
+let runningState = false;
+let loopId = 0;
+let btn = document.getElementById("btn");
 
-let game = setInterval(() => {
+function btnClassToggle() {
+  if (runningState) {
+    btn.className = "stop-btn";
+    btn.innerText = "Stop";
+  } else {
+    btn.className = "start-btn";
+    btn.innerText = "Start";
+  }
+}
+
+//setup
+btnInit();
+
+function btnInit() {
+  btnClassToggle();
+  btn.addEventListener("click", () => {
+    btnCtrl();
+  });
+}
+
+function btnCtrl() {
+  if (runningState) {
+    stopGame();
+  } else {
+    startGame();
+  }
+}
+
+function startGame() {
+  loopId = setInterval(gameLoop, 1500);
+  runningState = true;
+  btnClassToggle();
+}
+
+function stopGame() {
+  clearInterval(loopId);
+  runningState = false;
+  btnClassToggle();
+}
+
+//basic game loop
+function gameLoop() {
   let random1 = Math.ceil(Math.random() * 4);
   let random2 = Math.ceil(Math.random() * 4);
-  let selector = `.row-${random1} .cell-${random2}`;
-
-  let theDiv = document.querySelector(selector);
-  theDiv.append(mole);
+  let selector = `.row-${random1} .column-${random2}`;
+  let cell = document.querySelector(selector);
+  cell.append(mole);
+  removeMole(cell);
+}
+function removeMole(cell) {
   setTimeout(() => {
-    theDiv.removeChild(document.querySelector(".mole"));
+    cell.removeChild(document.querySelector(".mole"));
   }, 1000);
-}, 1500);
+}
